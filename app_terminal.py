b0c34582bd1c62bc2dadd31930aa023bd34f71ed
ChristalL99 <69207500+ChristalL99@users.noreply.github.com>
2022-02-22 12:34:02 +0800

    update lines for loading

--- app_terminal.py
+++ app_terminal.py
@@ -4,15 +4,16 @@ from flask import Flask, jsonify, request
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from github import Github
+import pickle
 
 # References: https://www.w3cschool.cn/flask/flask_http_methods.html
-# Discussed the idea with Zhengge Tang
+# Discussed the idea with Zhengge Tang, Chenglin Zhang
 
 # Creation of the Flask app
 app = Flask(__name__)
 
-g = Github()
-repo = g.get_repo("ChristalL99/COMPSCI401-project2")
+github = Github()
+repo = github.get_repo("ChristalL99/COMPSCI401-project2")
 model_commits = repo.get_commits(path = './text_clf.pickle')
 code_commits = repo.get_commits(path = './app_terminal.py')
 
@@ -23,12 +24,13 @@ model_date = str(pd.to_datetime(model_commits[0].commit.committer.date))
 model_url = 'https://github.com/ChristalL99/COMPSCI401-project2/blob/main/text_clf.pickle?raw=True'
 clf_filename, headers = urllib.request.urlretrieve(model_url, filename = "text_clf.pickle")
 
+model = pickle.load(open(clf_filename, 'rb'))
 
 @app.route('/api/american', methods=['POST'])
 def predict():
     content = request.get_json(force=True)
     text = [content['text']]
-    result = app.model.predict(text)[0]
+    result = model.predict(text)[0]
     return jsonify(is_american = str(result), version = version, model_date = model_date)
 
 if __name__ == '__main__':
